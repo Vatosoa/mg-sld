913bf9a17e442a887cc244faef9bb5526ee02da6
Vatosoa <razafindrazakav@gmail.com>
2024-03-06 14:54:24 +0300

    staticfiles

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "django-insecure-x^y1@733hqib#3pg$l9tmfo##2nfl2no(y3e)^2ox$yc%3jfuk
 DEBUG = True
 
 #ALLOWED_HOSTS = ['.vercel.app']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -67,15 +67,10 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
