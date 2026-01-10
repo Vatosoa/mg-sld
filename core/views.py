0dd89b35e7ad3b19dd4e12317c17bb52be075342
Vatosoa <razafindrazakav@gmail.com>
2026-01-10 10:17:51 +0100

    [UPD] Update views of POS & VOS result

--- core/views.py
+++ core/views.py
@@ -35,8 +35,7 @@ def postag_vos_view(request):
                     loaded_model_pos, loaded_token_encoder_pos, loaded_label_encoder_pos = load_pos_tagging_model()
                     predictions_pos = predict_pos_tags(loaded_model_pos, loaded_token_encoder_pos, loaded_label_encoder_pos, user_sentence)
                     tokenize_pos = tokenize_words_pos(user_sentence)
-                    predictions_pos_formatted = ' '.join([f"{token}/{pos}" for token, pos in zip(tokenize_pos, predictions_pos)])
-
+                    pos_results = list(zip(tokenize_pos, predictions_pos))
 
                     # VOS/EFL Structure
                     model_td, model_md, token_encoder_td, token_encoder_md, label_encoder_td, label_encoder_md = load_structure_vos_model()
@@ -46,11 +45,13 @@ def postag_vos_view(request):
                     # structure_prediction
                     if structure_prediction == "Mivadi-drafitra":
                         tokenize_vos = tokenize_words_md(user_sentence)
-                        predictions_vos_formatted = ' '.join([f"{token}/{vos}" for token, vos in zip(tokenize_words_md(user_sentence), predictions_vos)])
+                        # Créer une liste de tuples (token, vos) pour l'affichage avec flèches
+                        vos_results = list(zip(tokenize_words_md(user_sentence), predictions_vos))
                     else:
                         tokenize_vos = tokenize_words_td(user_sentence)
-                        predictions_vos_formatted = ' '.join([f"{token}/{vos}" for token, vos in zip(tokenize_words_td(user_sentence), predictions_vos)])
-                    # new_tokens
+                        # Créer une liste de tuples (token, vos) pour l'affichage avec flèches
+                        vos_results = list(zip(tokenize_words_td(user_sentence), predictions_vos))
+                    
                     fki = ' '.join(new_tokens) + '.'
 
 
@@ -60,19 +61,19 @@ def postag_vos_view(request):
                     predictions_summary = predict_summary(x_tokenizer, y_tokenizer, max_summary_len, loaded_encoder_model, loaded_decoder_model, reverse_target_word_index, user_sentence)
 
                     context = {
-                    'user_sentence': user_sentence, 
-                    'predictions_pos_formatted': predictions_pos_formatted, 
-                    'predictions_vos_formatted': predictions_vos_formatted, 
-                    'fki': fki, 
-                    'predictions_summary': predictions_summary, 
-                    'structure_prediction': structure_prediction,
-                    'error_message': error_message,
-                    'sentence_multiple_error': sentence_multiple_error,
+                        'user_sentence': user_sentence, 
+                        'pos_results': pos_results,
+                        'vos_results': vos_results,  # Nouvelle variable pour l'affichage VOS
+                        'fki': fki, 
+                        'predictions_summary': predictions_summary, 
+                        'structure_prediction': structure_prediction,
+                        'tokenize_vos': tokenize_vos,
+                        'error_message': error_message,
+                        'sentence_multiple_error': sentence_multiple_error,
                     }
 
                     return render(request, 'postag.html', context)
 
-                
                 except ValueError as e:
                     error_message = "Mialatsiny, some words in your input are not recognized in our database. Feel free to contact us via our Facebook/Twitter account for further assistance: Vatosoa Razafindrazaka"
                     context = {'error_message': error_message}
